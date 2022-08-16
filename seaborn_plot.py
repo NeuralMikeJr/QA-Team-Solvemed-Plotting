cd5ee22fde2b8782a4c98907b9603a5c371bb2b1
Katarzyna Dylewska <kad54@cam.ac.uk>
2022-08-16 13:31:42 +0200

    In progress
    Changed comments.

--- seaborn_plot.py
+++ seaborn_plot.py
@@ -5,16 +5,16 @@ import seaborn as sns
 
 def plot_hist(diff):
     sns.distplot(diff, bins=20)
-    plt.xlabel('Difference IBP [px]')
-    plt.ylabel('Number of frames IBP')
-    plt.title('Histogram of differences between adjacent frames IBP')
+    plt.xlabel('Difference [px]')
+    plt.ylabel('Number of frames')
+    plt.title('Histogram of differences between adjacent frames')
     plt.show()
 
 def smooth_plot(bbox_size):
     yhat = savgol_filter(bbox_size, 51, 3)
     sns.lineplot(x=range(len(yhat)), y=yhat)
     plt.legend()
-    plt.xlabel('Frame IBP')
-    plt.ylabel('Bbox size IBP [px]')
-    plt.title('Bbox size IBP')
+    plt.xlabel('Frame')
+    plt.ylabel('Bbox size [px]')
+    plt.title('Bbox size')
     plt.show()
